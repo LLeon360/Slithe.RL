3854df48a699f2a015639f401a3b12e57e8ab432
Leon Liu <liuleon360@gmail.com>
2025-01-28 19:17:56 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Create and activate conda environment
 conda create -y -n atari python=3.10
-source activate atari
+conda activate atari
 
 # Install main dependencies
 pip install gymnasium[atari,other] pettingzoo==1.24.3 stable-baselines3[extra] matplotlib numpy
