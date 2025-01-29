b3115b81dfe4c291e6893545d2fbb55e7075c4ce
LLeon360 <liuleon360@gmail.com>
2025-01-29 00:02:17 -0800

    fix shell script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Create and activate conda environment
 conda create -y -n atari python=3.10
-conda activate atari
+source activate atari
 
 # Install main dependencies
 pip install gymnasium[atari,other] pettingzoo==1.24.3 stable-baselines3[extra] matplotlib numpy
@@ -13,3 +13,4 @@ git clone https://github.com/Farama-Foundation/Multi-Agent-ALE.git
 cd Multi-Agent-ALE
 python setup.py build
 python setup.py install
+cd ..
